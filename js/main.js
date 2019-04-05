f3d90c25b11eb048873a38f7287a4cc769e300b0
Dongwook Shin <nawook0331@nate.com>
2019-04-06 02:04:22 +0900

    Site updated: 2019-04-06 02:04:20

--- js/main.js
+++ js/main.js
@@ -1,71 +1,45 @@
-(function(){
+(function($){
+    $('.article img:not(".not-gallery-item")').each(function () {
+        // wrap images with link and add caption if possible
+        if ($(this).parent('a').length === 0) {
+            $(this).wrap('<a class="gallery-item" href="' + $(this).attr('src') + '"></a>');
+            if (this.alt) {
+                $(this).after('<div class="has-text-centered is-size-6 has-text-grey caption">' + this.alt + '</div>');
+            }
+        }
+    });
 
-	// Highlight current nav item
-	var hasCurrent = false;
-
-	//把相对路径解析成绝对路径
-	function absolute(href) {
-	    var link = document.createElement("a");
-	    link.href = href;
-	    return (link.protocol+"//"+link.host+link.pathname+link.search+link.hash);
-	}
-
-	//移出所有的菜单的选中样式
-	$('#main-nav > li').each(function(){
-		$(this).removeClass('current-menu-item current_page_item');
-	});
-	var links = $('#main-nav > li > a');
-	var urls = window.location.href;
-	//为什么要从后面往前面遍历？因为首页极有可能是https://xxxxx/,
-	//这样的话肯定能够匹配所有的项
-	for (var i = links.length; i >= 0; i--) {
-		if(urls.indexOf(absolute(links[i])) != -1){
-			$(links[i]).parent().addClass('current-menu-item current_page_item');
-			//为什么还要设置hasCurrent？因为不排除首页是
-			//https://xxxx/index.html格式的
-			hasCurrent = true;
-			break;
-		}		
-	}
-
-
-	if (!hasCurrent) {
-		$('#main-nav > li:first').addClass('current-menu-item current_page_item');
-	}
-})();
-
-
-
-// article toc
-var toc = document.getElementById('toc')
-
-if (toc != null) {
-	window.addEventListener("scroll", scrollcatelogHandler);
-	var tocPosition = 194+25;
-
-	function scrollcatelogHandler(e) {
-		 var event = e || window.event,
-		     target = event.target || event.srcElement;
-		 var scrollTop = document.documentElement.scrollTop || document.body.scrollTop;
-		 if (scrollTop > tocPosition) {
-		     toc.classList.add("toc-fixed");
-		 } else {
-		     toc.classList.remove("toc-fixed");
-		 }
-	}
-}
-
-
-$('#main-navigation').on('click', function(){
-    if ($('#main-navigation').hasClass('main-navigation-open')){
-      $('#main-navigation').removeClass('main-navigation-open');
-    } else {
-      $('#main-navigation').addClass('main-navigation-open');
+    if (typeof(moment) === 'function') {
+        $('.article-meta time').each(function () {
+            $(this).text(moment($(this).attr('datetime')).fromNow());
+        });
     }
-  });
 
-$('#content').on('click', function(){
-    if ($('#main-navigation').hasClass('main-navigation-open')){
-      $('#main-navigation').removeClass('main-navigation-open');
+    function adjustNavbar() {
+        const navbarWidth = $('.navbar-main .navbar-start').outerWidth() + $('.navbar-main .navbar-end').outerWidth();
+        if ($(document).outerWidth() < navbarWidth) {
+            $('.navbar-main .navbar-menu').addClass('is-flex-start');
+        } else {
+            $('.navbar-main .navbar-menu').removeClass('is-flex-start');
+        }
     }
-  });
+    adjustNavbar();
+    $(window).resize(adjustNavbar);
+
+    var $toc = $('#toc');
+    if ($toc.length > 0) {
+    var $mask = $('<div>');
+        $mask.attr('id', 'toc-mask');
+
+        $('body').append($mask);
+
+        function toggleToc() {
+            $toc.toggleClass('is-active');
+            $mask.toggleClass('is-active');
+        }
+
+        $toc.on('click', toggleToc);
+        $mask.on('click', toggleToc);
+        $('.navbar-main .catalogue').on('click', toggleToc);
+    }
+})(jQuery);
